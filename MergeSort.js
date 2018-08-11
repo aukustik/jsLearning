28d29e743a53b9aebeea72537295081872a72ccf
aukustik <aukustik@yandex.ru>
2018-08-11 18:16:12 +0700

    Kek

--- MergeSort.js
+++ MergeSort.js
@@ -1,23 +1,50 @@
-class MergeSortClass
-{
-	function MergeSortClass()
+	var n = 0;
+	function ArrInput()
 	{
-		this.Arr = [];
+		Arr = [];
+		ArrTemp = [];
 		var InputValue = document.getElementById('InputMerge').value;
 		Arr = InputValue.split(' ');
-		console.log(this.Arr);
+		console.log(Arr);
+		CopyArray(Arr, 0, Arr.length, ArrTemp);
+		TDSplitMerge(ArrTemp, 0, Arr.length, Arr);
+		console.log(ArrTemp);
 	}
-	
-	function MergeSort(ArrayMain)
-	{
-		console.log(ArrayMain);
-		var n = ArrayMain.length;
-		console.log('Arrays length: ' + ArrayMain.length);
-		if (n == 1) return ArrayMain;
-		var Half1 = ArrayMain.splice(0, n/2);
-		var Half2 = ArrayMain.splice(n/2);
-		console.log('First Array:' + Half1);
-		console.log('Second Array:' + Half2);
-	}
-}
 
+	function TDSplitMerge(ArrayTemp, iBegin, iEnd, ArrayMain)
+	{
+		if (iEnd - iBegin < 2) return;
+		var iMiddle = ((iEnd + iBegin) / 2);
+		TDSplitMerge(ArrayMain, iBegin, iMiddle, ArrayTemp);
+		TDSplitMerge(ArrayMain, iMiddle, iEnd, ArrayTemp);
+		TopDownMerge(ArrayTemp, iBegin, iMiddle, iEnd, ArrayMain);
+	}
+
+	function TopDownMerge(ArrayMain, iBegin, iMiddle, iEnd, ArrayTemp)
+	{
+		var i = iBegin;
+		var j = iMiddle;
+		for (k = iBegin; k < iEnd; k++)
+		{
+			if (i < iMiddle && (j >= iEnd || ArrayMain[i]<=ArrayMain[j]))
+			{
+				ArrayTemp[k] = ArrayMain[i];
+				i = i + 1;
+			}
+			else
+			{
+				ArrayTemp[k] = ArrayMain[j];
+				j = j + 1;
+			}
+		}
+	}
+
+	function CopyArray(ArrayMain, iBegin, iEnd, ArrayTemp)
+	{
+		for (i = iBegin; i < iEnd; i++)
+		{
+			ArrayTemp[i] = ArrayMain[i];
+		}
+		console.log(ArrayMain);
+		console.log(ArrayTemp);
+	}
